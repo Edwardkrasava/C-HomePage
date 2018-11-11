e753c48e8d9a16f602946b16b5b91c6707c1d890
Edwardkrasava <golineyden@gmail.com>
2018-11-11 23:50:36 +0200

    11 11 2018

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+
+
+    $(window).resize(function (event) {
+        console.log(event);
+        if (event.target.innerWidth < 864) {
+            $('.right__content').insertAfter('.pups');
+        } else {
+            $('.right__content').insertAfter('.left__content');
+        }
+    });
+
+    $('#menu_icon').on('click', function (event) {
+        event.preventDefault();
+        $('.container__navigation').toggle(800);
+    });
+
+
+
+});
